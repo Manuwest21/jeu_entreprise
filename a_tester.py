cf0cca4dc2d33c341e3726dd6378ce6356d18a25
Evgeni1494 <yoni1@hotmail.fr>
2023-01-19 10:49:14 +0100

    Ajout de test des fonctions

--- a_tester.py
+++ a_tester.py
@@ -51,7 +51,7 @@ def fin_de_partie(dico_jeu):
      
 # FONCTIONS MANU    
             
-def choix_joueur():    #choix d'attaquer ou  utiliser potion.
+def choix_joueur(dico_jeu):    #choix d'attaquer ou  utiliser potion.
     """
     args: ne prend pas d'argument
     demande au joueur s'il souhaite attaquer ou prendre la potion
@@ -60,15 +60,18 @@ def choix_joueur():    #choix d'attaquer ou  utiliser potion.
     
     choix=0   
     if dico_jeu["potions"]<1:
+        return "Tested"
         choix=="attaquer"
-    while choix not in ["potion", "attaquer"]:
-        choix="attaquer"
-    # if choix =="attaquer":
-    #     def (attaque)
-    # elif choix=="potion":
-    #     def (potion)
-    dico_jeu["tour_joueur"]-=1
-    return choix
+    elif  dico_jeu["potions"]:
+        while choix not in ["boire", "attaquer"]:
+            return None
+            choix=input("tu veux attaquer ou boire une potion? [attaquer/boire]:     ")
+    if choix =="attaquer":
+        return True
+        attaque_joueur(dico_jeu)
+    elif choix=="boire":
+        return False
+        usage_potions(dico_jeu)
 
 def usage_potions():
     """
@@ -85,38 +88,46 @@ def usage_potions():
     dico_jeu["points_de_vie_joueur"]+=pts_potion
     print(f"vous avez gagné {pts_potion} de vie!")
     return True
+
+def à_qui(dico_jeu):
+    if dico_jeu["tour_joueur"]==dico_jeu["tour_ennemi"]:  
+        return True
+    elif dico_jeu["tour_ennemi"]<dico_jeu["tour_joueur"]:
+        return False
     
 # ====================================================================================================================================================
 
 
 # FONCTIONS YVETTE
 
-def attaque_joueur():
-    """fonction qui permet au joueur d'attaquer l'ennemi
-
-    Returns:
-        il affiche le nombre de dégas subis et le nombre de points de vie restant à l'ennemi
+def attaque_joueur(dico_jeu):
+    """
+    args: ne prend pas d'arguments
+    fonction qui permet au joueur d'attaquer l'ennemi
+    il affiche un nombre aléatoire de dégas subis entre 5 et 10 et le nombre de points de vie restant à l'ennemi
     """
     # fonction attaque qui enlève les points à l'ennemi de manière aléatoire
-    if dico_jeu["points_de_vie_joueur"]>0:
-        degats_subis_ennemi = rd.randint(5,10)
-        dico_jeu["points_de_vie_ennemi"]-= degats_subis_ennemi
-    print(f'vous avez infligé {degats_subis_ennemi} dégâts.') 
+    # if dico_jeu["points_de_vie_joueur"]>0:
+    dico_jeu["tour_joueur"]+=1
+    degats_subis_ennemi = rd.randint(5,10)
+    dico_jeu["points_de_vie_ennemi"]-= degats_subis_ennemi
+    print(f"vous avez infligé {degats_subis_ennemi} dégâts à l'ennemi") 
     print(f'il reste {dico_jeu["points_de_vie_ennemi"]} points de vie à votre ennemi')
     
     
-def attaque_ennemi():
-    """la fonction permet à l'ennemi d'attaquer le joueur
-    
-    Returns:
-        il affiche le nombre de dégas subis et le nombre de points de vie restants au joueur
+def attaque_ennemi(dico_jeu):
+   
     """
-    if dico_jeu["points_de_vie_ennemi"]>0:
-        degats_subis_joueur = rd.randint(5,15)
-        dico_jeu["points_de_vie_joueur"]-=degats_subis_joueur
-        print(f'vous avez subi {degats_subis_joueur} dégâts.') 
-    print(f'il vous reste {dico_jeu["points_de_vie_joueur"]} points de vie')
-    dico_jeu["tour_ennemi"]-=1
+    args: ne prend pas d'arguments
+    la fonction permet à l'ennemi d'attaquer le joueur
+    il affiche un nombre aléatoire de dégas subis et le nombre de points de vie restants au joueur
+    """
+    # if dico_jeu["points_de_vie_ennemi"]>0:
+    dico_jeu["tour_ennemi"]+=1
+    degats_subis_joueur = rd.randint(5,15)
+    dico_jeu["points_de_vie_joueur"]-=degats_subis_joueur
+    print(f'-                                                   -vous avez subi {degats_subis_joueur} dégâts.') 
+    print(f'-                                                   -il vous reste {dico_jeu["points_de_vie_joueur"]} points de vie')
 
 # =======================================================================================================================================================
 
--- test_fonctions.py
+++ test_fonctions.py
@@ -31,26 +31,70 @@ class TestAddWithUnittest(unittest.TestCase):
      "score" : 0
  }), False )
         
-
+    # Test de la fonction choix_joueur
     def test_choix_joueur(self):
-        self.assertEqual(choix_joueur(),"attaquer")
-
-
-    def test_usage_potion(self):
+        self.assertNotIn("test_item",["boire","attaquer"],None)
+        self.assertEqual(choix_joueur(dico_jeu = {
+     "points_de_vie_joueur" : 50,
+     "points_de_vie_ennemi" : 50,
+     "potions" : 0,
+     "tour_joueur" : 1,
+     "tour_ennemi" : 0,
+     "score" : 0
+ }), "Tested")
+        self.assertEqual(choix_joueur(dico_jeu = {
+     "points_de_vie_joueur" : 50,
+     "points_de_vie_ennemi" : 50,
+     "potions" : 1,
+     "tour_joueur" : 1,
+     "tour_ennemi" : 0,
+     "score" : 0
+ }), None)
+       
+    # Test usage_potions
+    def test_usage_potions(self):
         self.assertEqual(usage_potions(), True)
 
-    def test_attaque_jouer(self):
-        self.
+    # Test a_qui
+    def test_a_qui(self):
+        self.assertEqual(à_qui(dico_jeu = {
+     "points_de_vie_joueur" : 50,
+     "points_de_vie_ennemi" : 50,
+     "potions" : 1,
+     "tour_joueur" : 1,
+     "tour_ennemi" : 1,
+     "score" : 0
+ }), True)
+        self.assertEqual(à_qui(dico_jeu = {
+     "points_de_vie_joueur" : 50,
+     "points_de_vie_ennemi" : 50,
+     "potions" : 1,
+     "tour_joueur" : 1,
+     "tour_ennemi" : 0,
+     "score" : 0
+ }), False)
 
+    def test_attaque_joueur(self):
+        self.assertEqual(à_qui(dico_jeu = {
+     "points_de_vie_joueur" : 50,
+     "points_de_vie_ennemi" : 50,
+     "potions" : 1,
+     "tour_joueur" : 1,
+     "tour_ennemi" : 1,
+     "score" : 0
+ }), True)
+    
+    def test_attaque_ennemi(self):
+        self.assertEqual(à_qui(dico_jeu = {
+     "points_de_vie_joueur" : 50,
+     "points_de_vie_ennemi" : 50,
+     "potions" : 1,
+     "tour_joueur" : 1,
+     "tour_ennemi" : 1,
+     "score" : 0
+ }), True)
 
-
-
-
-
-
-
-
-
+    
 
 
 
